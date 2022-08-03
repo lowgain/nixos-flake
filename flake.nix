8411ab506afbc34c966fb0c03cb96e98de2b23a9
papa-marshmellow <logan.t2020@tutanota.com>
2022-08-03 19:46:34 -0400

    changed channel release

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Logans personal flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
